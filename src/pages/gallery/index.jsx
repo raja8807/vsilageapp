4638f44b75c3316b49b306a43b50e17b451a7fa6
raja8807 <107910012+raja8807@users.noreply.github.com>
2023-09-07 01:15:32 +0530

    Update index.jsx

--- src/pages/gallery/index.jsx
+++ src/pages/gallery/index.jsx
@@ -1,8 +1,17 @@
 import GalleryScreen from "@/components/pages/gallery/gallery";
 
-const GalleryPage = ()=>{
-    return <GalleryScreen/>
+const GalleryPage = ({data})=>{
+    return <GalleryScreen data={data}/>
 }
 
 export default GalleryPage;
+
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch(`api/image`)
+  const data = await res.json()
+ 
+  // Pass data to the page via props
+  return { props: { data } }
+}
 
